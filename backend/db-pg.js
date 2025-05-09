5ba280294bf835f507f3d838d6f03858092be55c
Fernanda Piotto <94077007+fefeled@users.noreply.github.com>
2025-05-09 01:38:32 -0300

    Update db-pg.js
    adicionei códigos da linha 1 até a linha 6

--- backend/db-pg.js
+++ backend/db-pg.js
@@ -1,3 +1,9 @@
+import postgres from 'postgres'
+
+const connectionString = process.env.DATABASE_URL
+const sql = postgres(connectionString)
+
+export default sql
 const { Pool } = require('pg');
 
 const pool = new Pool({
