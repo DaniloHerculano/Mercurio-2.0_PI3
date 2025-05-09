4337848587ccb1ee19ae6561be117342707ed46e
Fernanda Piotto <94077007+fefeled@users.noreply.github.com>
2025-05-09 01:44:01 -0300

    Update db-pg.js
    retirei o que tinha adicionado, volta a estaca zero

--- backend/db-pg.js
+++ backend/db-pg.js
@@ -1,19 +1,13 @@
-import postgres from 'postgres'
-
-const connectionString = process.env.DATABASE_URL=postgresql:/postgres.cahtamyswdujwrjlfkfg:[Ferroviagravata12#]@aws-0-sa-east-1.pooler.supabase.com:6543/postgres
-const sql = postgres(connectionString)
-
-export default sql
 const { Pool } = require('pg');
 
 const pool = new Pool({
   host: 'aws-0-sa-east-1.pooler.supabase.com',
-  user: 'postgres.cahtamyswdujwrjlfkfg',         // usually PostgreSQL default user is 'postgres'
+  user: 'postgres.cahtamyswdujwrjlfkfg',
   password: 'Ferroviagravata12#',
   database: 'postgres',
-  port: 6543,            // default port for PostgreSQL
-  max: 15,               // maximum number of connections
-  idleTimeoutMillis: 30000, // optional: how long a client is allowed to remain idle
+  port: 6543,
+  max: 15,
+  idleTimeoutMillis: 30000,
 });
 
 module.exports = pool;
