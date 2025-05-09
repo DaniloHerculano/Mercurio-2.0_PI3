62e128c77aa217a1dd4bf7ce9b78234582cb44cb
Fernanda Piotto <94077007+fefeled@users.noreply.github.com>
2025-05-09 01:06:10 -0300

    Update pedidos.js
    alterei algumas partes como a linha 19 e 20 para o formato que o PostgreSQL aceita.

--- backend/routes/pedidos.js
+++ backend/routes/pedidos.js
@@ -11,19 +11,20 @@ router.post('/', async (req, res) => {
       total, status, dataPedido, dias
     } = req.body;
 
-    const [result] = await pool.query(`
+    const result = await pool.query(`
       INSERT INTO pedidos 
       (datepickerstart, datepickerend, modelo, quantidadebancos, quantidadecadeiras, quantidademesas,
       endereco, tipo_entrega, totaldiaria, totalpedido_nfrete, valor_frete,
       totalpedido, status, data_pedido, dias)
-      VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)
+      RETURNING id
     `, [
       dataInicio, dataRetorno, modelo, bancos, cadeiras, mesas,
       endereco, tipoEntrega, totalDiario, totalSemFrete, frete,
       total, status, dataPedido, dias
     ]);
-
-    res.status(201).json({ success: true, pedidoId: result.insertId });
+    
+    res.status(201).json({ success: true, pedidoId: result.rows[0].id });
   } catch (error) {
     console.error('Erro ao criar pedido:', error);
     res.status(500).json({ success: false, message: 'Erro no servidor' });
