8baba3e4ad9e23408c3d2ce541b22fdb3cd0eadb
Fernanda Piotto <94077007+fefeled@users.noreply.github.com>
2025-05-09 21:21:12 -0300

    Update server.js
    mais um teste, comentei a parte do código que estava antes, caso de tudo errado

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/*const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
@@ -38,4 +38,26 @@ const corsOptions = {
   methods: 'GET,POST',
   optionsSuccessStatus: 200
 };
-app.use(cors(corsOptions));
+app.use(cors(corsOptions)); */
+
+//TESTE FERNANDA
+
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const pedidoRoutes = require('./routes/pedidos');
+const estoqueRoutes = require('./routes/estoque');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use('/api/pedidos', pedidoRoutes);
+app.use('/api/estoque', estoqueRoutes);
+
+const PORT = 6543;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
