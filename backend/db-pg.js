aafb33fb3b589d15220a0497e5552715edf7398b
Fernanda Piotto <94077007+fefeled@users.noreply.github.com>
2025-05-12 00:20:39 -0300

    Update db-pg.js
    mudei o código pois o banco de dados não estava hospedado em um servidor, o netlify não consegue ser host de banco de dados, comentei o código antigo.

--- backend/db-pg.js
+++ backend/db-pg.js
@@ -1,3 +1,4 @@
+/* //ANTIGO (NAO ESTAVA ALOCADO NUM SERVIDOR)
 const { Pool } = require('pg');
 
 const pool = new Pool({
@@ -6,6 +7,23 @@ const pool = new Pool({
   password: 'Ferroviagravata12#',
   database: 'postgres',
   port: 6543,
+  max: 15,
+  idleTimeoutMillis: 30000,
+});
+
+module.exports = pool; */
+
+//NOVO - UTILIZANDO O RENDER 
+
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
+  ssl: { rejectUnauthorized: false },
   max: 15,
   idleTimeoutMillis: 30000,
 });
