f3eade270ccd9876dfdbbdb8fbb63d04bee12edd
Fernanda Piotto <94077007+fefeled@users.noreply.github.com>
2025-05-21 00:55:17 -0300

    Update db-pg.js
    tirei as partes comentadas para limpar o código.

--- backend/db-pg.js
+++ backend/db-pg.js
@@ -1,20 +1,3 @@
-/* //ANTIGO (NAO ESTAVA ALOCADO NUM SERVIDOR)
-const { Pool } = require('pg');
-
-const pool = new Pool({
-  host: 'aws-0-sa-east-1.pooler.supabase.com',
-  user: 'postgres.cahtamyswdujwrjlfkfg',
-  password: 'Ferroviagravata12#',
-  database: 'postgres',
-  port: 6543,
-  max: 15,
-  idleTimeoutMillis: 30000,
-});
-
-module.exports = pool; */
-
-//NOVO - UTILIZANDO O RENDER 
-
 const { Pool } = require('pg');
 
 const pool = new Pool({
